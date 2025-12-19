29a5ca0a5093f3345da282a703ee41c9527b6ba1
Dr.Error <56202075+KnsGoyoLV@users.noreply.github.com>
2025-12-19 14:24:28 +0200

    Menu start punkts

--- Autoserviss.java
+++ Autoserviss.java
@@ -4,9 +4,35 @@ import java.util.ArrayList;
 
 public class Autoserviss {
     public static void main(String[] args) {
-        Car myCar = new Car();
-        Engine myEngine = new Engine();
-        Gearbox myGearbox = new Gearbox();
-        Wheel myWheel = new Wheel();
+       while(true){
+            String[] options = {
+                    "Create car",
+                    "List cars",
+                    "Delete car",
+                    "Save cars to file",
+                    "View saved file",
+                    "Exit"
+            };
+            int choice = JOptionPane.showOptionDialog(
+                    null,
+                    "Autoserviss Menu",
+                    "Autoserviss",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    options,
+                    options[0]
+            );
+             if (choice == 0)       ;
+             else if (choice == 1)  ;
+             else if (choice == 2)  ;
+             else if (choice == 3)  ;
+             else if (choice == 4)  ;
+             else break;
+            
+
+
+
+       }
     }
 }
