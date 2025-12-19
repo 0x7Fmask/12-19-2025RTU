fa30544106b5dfc13a1e1f9e687e05c033d0ca84
Dr.Error <56202075+KnsGoyoLV@users.noreply.github.com>
2025-12-19 14:20:46 +0200

    Sākums sagatavot

---
+++ Autoserviss.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Autoserviss {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Engine myEngine = new Engine();
+        Gearbox myGearbox = new Gearbox();
+        Wheel myWheel = new Wheel();
+    }
+}
